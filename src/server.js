a0d93931602faa2f4314f3950f5c74d4379ff79e
Md Faisal Ahmed <122660563+FaisalAhmed025@users.noreply.github.com>
2024-03-07 12:15:58 +0600

    change port

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 import config from "./config/index";
 import app from "./mainapp";
 
+
 async function mainserver() {
-  app.listen(config.port, () => {
-    console.log(`Application listening on port ${config.port}`);
+  app.listen(3000, () => {
+    console.log(`Application listening on port 3000`);
   });
 }
 
