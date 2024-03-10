c7db3da0b5a730fa08a7081904a6d5690f75e46d
Md Faisal Ahmed <122660563+FaisalAhmed025@users.noreply.github.com>
2024-03-10 12:58:58 +0600

    change db

--- src/database/db.js
+++ src/database/db.js
@@ -1,16 +1,16 @@
 import mysql from 'mysql2/promise'
 const db = {
-    port: 3306,
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'flyfarexpress',
+    // port: 3306,
+    // host: '127.0.0.1',
+    // user: 'root',
+    // password: '',
+    // database: 'flyfarexpress',
 
 
-    // host: 'flyfarint.com',
-    // user: 'flyfarin_fflv2',
-    // password: '123Next2$',
-    // database: 'flyfarin_fflv2',
+    host: 'flyfarint.com',
+    user: 'flyfarin_fflv2',
+    password: '123Next2$',
+    database: 'flyfarin_fflv2',
 };
 export const pool = mysql.createPool(db);
 
