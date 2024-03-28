cc9163ee8dd5cfc8f2dcb1c9adf44a4a1e9b2bf2
Md Faisal Ahmed <122660563+FaisalAhmed025@users.noreply.github.com>
2024-03-28 10:45:51 +0600

    update

--- src/booking/bookingRoute.js
+++ src/booking/bookingRoute.js
@@ -3,7 +3,7 @@ import express from 'express';
 import { bookingController } from './bookingcontroller';
 const router = express.Router();
 
-router.post("/book/packid/:PkID/userid/:id", bookingController.Book$Hold)
+router.post("/book/packid/:PKID/userid/:id", bookingController.Book$Hold)
 router.get("/book/allbooking", bookingController.getAllBookings)
 router.get("/book/single/:bookingid", bookingController.getSingleBookings)
 router.get("/book/userid/:userid", bookingController.getuserBookings)
