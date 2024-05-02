c3e1940d5254de1ed7d36b76510cad2e9d7393ee
Md Faisal Ahmed <122660563+FaisalAhmed025@users.noreply.github.com>
2024-05-02 09:51:48 +0600

    update

--- src/blog/route.js
+++ src/blog/route.js
@@ -9,7 +9,7 @@ const router  = express.Router()
 router.post('/addblog',  upload.fields([{name:'blogimages', maxCount:10}, {name:'secondimage', maxCount:10}]),  handleblogImage, blogcontroller.addBlog)
 router.get('/all', blogcontroller.getallblogs)
 router.delete('/:id', blogcontroller.deleteBlog)
-router.patch('/update/:id', upload.single('image'), imageHandlerUpdate, blogcontroller.updateBlogcontroller)
+router.patch('/update/:id', upload.single('secondimage'), imageHandlerUpdate, blogcontroller.updateBlogcontroller)
 router.patch('/blogid/:id/urlid/:urlid',  upload.single('image'), imageHandlerUpdate,  blogcontroller.updateimages)
 
 
