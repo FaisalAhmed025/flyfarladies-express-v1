01ff846fdf10f0acf559f69dc6e9eda99335f6c9
Md Faisal Ahmed <122660563+FaisalAhmed025@users.noreply.github.com>
2024-05-19 12:43:59 +0600

    update

--- src/SSL/router.js
+++ src/SSL/router.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import { sslpaymentController } from './controller';
 const router  = express.Router()
-router.get('/init/:id' ,  sslpaymentController.initpayment)
+router.post('/init/:id' ,  sslpaymentController.initpayment)
 router.post('/success/:tran_id/:id', sslpaymentController.success)
 router.get('/validate/:val_id', sslpaymentController.validate)
 router.post('/refund-request', sslpaymentController.RefundRequest)
